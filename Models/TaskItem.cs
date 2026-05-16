88b9efa93468bae5e194859f985ebe97c8341ae7
Brigita <brigitaalenskaite@gmail.com>
2026-05-17 00:49:06 +0300

    Added TaskItem model and TaskRepository

---
+++ Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskFlow.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ Repositories/TaskRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TaskFlow.Models;
+
+namespace TaskFlow.Repositories
+{
+    public class TaskRepository
+    {
+        private List<TaskItem> tasks = new List<TaskItem>();
+
+        public List<TaskItem> GetAll()
+        {
+            return tasks;
+        }
+
+        public void AddTask(TaskItem task)
+        {
+            tasks.Add(task);
+        }
+    }
+}
